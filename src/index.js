0289221c230d12ffb762b2fcf7de3add9f582284
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-23 04:00:59 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div>
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn3.gstatic.com%2Flicensed-image%3Fq%3Dtbn%3AANd9GcRDiAJs9euYV1-6JlnH9o-lL01D3rpduCiQuqaJkZ9BqQr0iVwX2OXOKQJykm-o7qvWcTmDiFkdFNiElZQ&psig=AOvVaw1QTyUp83q4Gwlz2oGQQkZG&ust=1687556238428000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLD3-IDr1_8CFQAAAAAdAAAAABAE"/>
+<h1>Lizard</h1>
+<p>Lizards are a widespread group of squamate reptiles, with over 7,000 species, ranging across all continents except Antarctica</p>
+  <a>SHARE</a>  <a>LEARN MORE</a>
+</div>
+`;
